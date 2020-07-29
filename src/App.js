5f9381a20e976afe7aa922379df7ceb10f3d20c2
Azlina Yeo <azlinayeo@Azlinas-MBP.lan>
2020-07-30 00:01:05 +0100

    coded dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  FormControl,
+  Select,
+  MenuItem
+} from '@material-ui/core';
 import './App.css';
 
+//
+
 function App() {
+  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+  const [country, setCountry] = useState('worldwide');
+  useEffect(() => {
+    const getCountriesData = async () => {
+      try {
+        const data = await axios.get("https://disease.sh/v3/covid-19/countries");
+        const arrCountries = data.data.map(item => ({
+
+          name: item.country,
+          value: item.countryInfo.iso2,
+
+        }))
+        setCountries(arrCountries);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getCountriesData();
+  }, [])
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value={country}
+            onChange={onCountryChange}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map(({ value, name }, index) => (
+              <MenuItem key={index} value={value}>{name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div >
+      <div className="app__stats">
+
+      </div>
+    </div >
   );
 }
 
